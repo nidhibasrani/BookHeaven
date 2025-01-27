5bdf079e8f437ea53c62f92271d922aea56919d1
Nidhi <nidhibasrani54@gmail.com>
2025-01-27 13:34:00 +0530

    changes

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -22,7 +22,7 @@ const Profile = () => {
     } else {
       const fetch = async () => {
         const response = await axios.get(
-          "http://localhost:1000/api/v1/getUserData",
+          `${import.meta.env.VITE_SERVER_URL}/getUserData`,
           { headers }
         );
         setProfileData(response.data);
